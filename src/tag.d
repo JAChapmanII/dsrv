c757124260a813bd27f084d281384011b763b742
Jeff Chapman II <jac@JAChapmanII.net>
2011-05-14 12:39:35 -0400

    Simplify logic for tags which have simple endings (nearly everything)

--- src/tag.d
+++ src/tag.d
@@ -4,17 +4,18 @@ import std.conv;
 class Tag {
 	public:
 		this(bool isSingle = false) {
-			this.start = "";
 			this.single = isSingle;
-			this.end = "";
-			this.content = "";
 		}
 
 		void print(string indent = "") {
 			if((this._content.length > 0) &&
 				(this.subTags.length == 0)) {
-				writeln(indent ~ "<" ~ this.start ~ ">" ~
-						this._content ~ "<" ~ this.end ~ ">");
+				write(indent ~ "<" ~ this.start ~ ">" ~
+						this._content);
+				if(this.end.length == 0)
+					writeln("</" ~ this.start ~ ">");
+				else
+					writeln("<" ~ this.end ~ ">");
 				return;
 			}
 			write(indent ~ "<" ~ this.start);
@@ -33,7 +34,10 @@ class Tag {
 			foreach(tag; this.subTags)
 				tag.print(indent ~ "  ");
 
-			writeln(indent ~ "<" ~ this.end ~ ">");
+			if(this.end.length == 0)
+				writeln(indent ~ "</" ~ this.start ~ ">");
+			else
+				writeln(indent ~ "<" ~ this.end ~ ">");
 		}
 
 		void add(Tag nTag) {
@@ -57,27 +61,17 @@ class Tag {
 class HTML : Tag {
 	public:
 		this() {
-			super();
-
-			this.start = "?xml version = \"1.0\" encoding = \"utf-8\" ?>\n"
-				"<!DOCTYPE html\n" ~
-					"\tPUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n" ~
-					"\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\n" ~
-				"<html";
+			this.start = "html";
 			this.attributes["xmlns"] = "http://www.w3.org/1999/xhtml";
 			this.attributes["xml:lang"] = "en";
 			this.attributes["lang"] = "en";
-			this.end = "/html";
 		}
 }
 
 class Head : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "head";
-			this.end = "/head";
 		}
 }
 
@@ -95,70 +89,49 @@ class Meta : Tag {
 class Title : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "title";
-			this.end = "/title";
 		}
 }
 
 class Body : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "body";
-			this.end = "/body";
 		}
 }
 
 class Heading : Tag {
 	public:
 		this(int iLevel) {
-			super();
-
 			this.start = "h" ~ toImpl!(string)(iLevel);
-			this.end = "/h" ~ toImpl!(string)(iLevel);
 		}
 }
 
 class Paragrah : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "p";
-			this.end = "/p";
 		}
 }
 
 class UnorderedList : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "ul";
-			this.end = "/ul";
 		}
 }
 
 class OrderedList : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "ol";
-			this.end = "/ol";
 		}
 }
 
 class ListElement : Tag {
 	public:
 		this() {
-			super();
-
 			this.start = "li";
-			this.end = "/li";
 		}
 }
 
