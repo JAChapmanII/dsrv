0653e8135d54b63c84d1b3c547917aeb02b89978
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-07 10:44:29 -0400

    Make a table of available repositories, kind of hacky

--- src/code_handler.d
+++ src/code_handler.d
@@ -1,5 +1,7 @@
 import std.xml;
 
+import repository;
+
 Element codeHandler(string URL) {
 	Element mMColumn = new Element("div");
 	mMColumn.tag.attr["class"] = "mcol";
@@ -7,6 +9,20 @@ Element codeHandler(string URL) {
 	mBody.tag.attr["class"] = "scol";
 
 	mBody ~= new Element("h3", URL);
+	Repository[] repos = Repository.parseRepositories();
+	if(!repos.length) {
+		mBody ~= new Element("p", "There are no repositories");
+	} else {
+		Element rTable = new Element("table");
+		foreach(repo; repos) {
+			Element rRow = new Element("tr");
+			rRow ~= new Document("<td>" ~ repo.language() ~ "&#160;&#160;</td>");
+			rRow ~= new Document("<td>" ~ repo.name() ~ "&#160;&#160;</td>");
+			rRow ~= new Element("td", repo.description());
+			rTable ~= rRow;
+		}
+		mBody ~= rTable;
+	}
 
 	mMColumn ~= mBody;
 	return mMColumn;
