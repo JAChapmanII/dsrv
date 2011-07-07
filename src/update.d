fb182b5652d8c652387b7cec77c39a80ee29d5f4
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-06 22:14:16 -0400

    Parse udates file to get available posts, make getContent work

--- src/update.d
+++ src/update.d
@@ -1,4 +1,6 @@
 import std.file;
+import std.string;
+import std.conv;
 
 static const string UDATES_FILE = "udates";
 static const string UDATES_DIR = "updates";
@@ -15,13 +17,23 @@ class Update {
 		string getContents() {
 			if(!isDir(UDATES_DIR))
 				return "udates dir is nonexistant";
-			return "contents";
+			string fName = UDATES_DIR ~ "/" ~ zfill(to!string(this.number), 4);
+			if(!isFile(fName))
+				return "update does not exist";
+
+			string contents = readText(fName);
+			return contents;
 		}
 
 		static Update[] parseUDates() {
 			Update[] updates;
 			if(!isFile(UDATES_FILE))
 				return updates;
+			foreach(line; splitlines(readText(UDATES_FILE))) {
+				string[] fields = split(line, "|");
+				updates ~= new Update(
+						to!int(fields[0]), fields[1], fields[2], fields[3]);
+			}
 			return updates;
 		}
 
