90d8c91c272f2cba9e6d6a9c2f9097fab04c8254
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-07 13:35:22 -0400

    Add function to get post header, add handling of update/all

--- src/update_handler.d
+++ src/update_handler.d
@@ -14,57 +14,74 @@ Element updateHandler(string URL) {
 	mBody.tag.attr["class"] = "scol";
 	mMColumn ~= mBody;
 
+	Element spacer = new Document("<p>&#160;</p>");
+	spacer.tag.attr["style"] = "line-height:0;clear:both";
+
 	mBody ~= new Element("h3", URL);
 	Update[] updates = Update.parseUDates();
 	if(!updates.length) {
 		mBody ~= new Element("p", "There are no updates");
 	} else {
-		string suffix = URL[URL_PREFIX.length..$];
-		int r = cast(int)updates.length + 1;
-		if(isNumeric(suffix)) {
-			r = to!int(suffix) - 1;
+		if((URL == "update") || (URL == "update/all")) {
+			for(long i = updates.length - 1; i >= 0; --i) {
+				Element post;
+				if(i == updates.length - 1)
+					post = formatUpdate(updates[i], "update ubblue");
+				else if(i < updates.length - 3)
+					post = formatPostHeader(updates[i]);
+				else
+					post = formatUpdate(updates[i], "update ubox");
+
+				if(!(post is null))
+					mBody ~= post;
+			}
 		} else {
-			string title = URL[URL_PREFIX.length..$];
-			for(int i = 0; i < updates.length; ++i)
-				if(updates[i].title == title)
-					r = i;
-		}
+			string suffix = URL[URL_PREFIX.length..$];
+			int r = cast(int)updates.length + 1;
+			if(isNumeric(suffix)) {
+				r = to!int(suffix) - 1;
+			} else {
+				string title = URL[URL_PREFIX.length..$];
+				for(int i = 0; i < updates.length; ++i)
+					if(updates[i].title == title)
+						r = i;
+			}
 
-		if(r >= updates.length) {
-			mBody ~= new Element("p", "Couldn't find post, sorry!");
-			Element latestLink = new Element("a", "Latest post");
-				latestLink.tag.attr["href"] = 
-					URL_BASE ~ URL_PREFIX ~ to!string(updates.length);
-			mBody ~= latestLink;
-			return mMColumn;
-		}
-		Update update = updates[r];
+			if(r >= updates.length) {
+				mBody ~= new Element("p", "Couldn't find post, sorry!");
+				Element latestLink = new Element("a", "Latest post");
+					latestLink.tag.attr["href"] = 
+						URL_BASE ~ URL_PREFIX ~ to!string(updates.length);
+				mBody ~= latestLink;
+				return mMColumn;
+			}
+			Update update = updates[r];
 
-		Element post = formatUpdate(update);
+			Element post = formatUpdate(update);
+			if(!(post is null))
+				mBody ~= post;
 
-		int next = r + 2, prev = r;
-		if(prev >= 1) {
-			Element prevE = new Element("p");
-			prevE.tag.attr["class"] = "lcol nlink";
-			Element prevL = new Element("a", "Previous");
-			prevL.tag.attr["href"] = 
-				URL_BASE ~ URL_PREFIX ~ to!string(prev);
-			prevE ~= prevL;
-			mBody ~= prevE;
+			int next = r + 2, prev = r;
+			if(prev >= 1) {
+				Element prevE = new Element("p");
+				prevE.tag.attr["class"] = "lcol nlink";
+				Element prevL = new Element("a", "Previous");
+				prevL.tag.attr["href"] = 
+					URL_BASE ~ URL_PREFIX ~ to!string(prev);
+				prevE ~= prevL;
+				mBody ~= prevE;
+			}
+			if(next <= updates.length) {
+				Element nextE = new Element("p");
+				nextE.tag.attr["class"] = "rcol nlink";
+				nextE.tag.attr["style"] = "text-align:right";
+				Element nextL = new Element("a", "Next");
+				nextL.tag.attr["href"] = 
+					URL_BASE ~ URL_PREFIX ~ to!string(next);
+				nextE ~= nextL;
+				mBody ~= nextE;
+			}
 		}
-		if(next <= updates.length) {
-			Element nextE = new Element("p");
-			nextE.tag.attr["class"] = "rcol nlink";
-			nextE.tag.attr["style"] = "text-align:right";
-			Element nextL = new Element("a", "Next");
-			nextL.tag.attr["href"] = 
-				URL_BASE ~ URL_PREFIX ~ to!string(next);
-			nextE ~= nextL;
-			mBody ~= nextE;
-		}
-		
-		Element spacer = new Document("<p>&#160;</p>");
-		spacer.tag.attr["style"] = "line-height:0;clear:both";
 		mBody ~= spacer;
 	}
 
@@ -75,22 +92,12 @@ Element formatUpdate(Update update, string wrapperClass = "update") {
 	if(update is null)
 		return null;
 
-	Element post;
-	string c = 
-		"<div class=\"" ~ wrapperClass ~ "\">" ~ update.getContents() ~ "</div>";
+	Element post = new Element("div");
+		post.tag.attr["class"] = wrapperClass;
+	string c = "<div class=\"post\">" ~ update.getContents() ~ "</div>";
 	try {
 		check(c);
-		Element postHeader = new Element("h3");
-		postHeader ~= new Document("<span>&#160;</span>");
-			postHeader.tag.attr["class"] = "uhead";
-		Element pTitle = new Element("a", " " ~ update.title);
-			pTitle.tag.attr["class"] = "utitle lcol";
-		Element pDT = new Element("span",
-				update.date ~ " " ~ update.time);
-			pDT.tag.attr["class"] = "udt rcol";
-		postHeader ~= pTitle;
-		postHeader ~= pDT;
-		post ~= postHeader;
+		post ~= formatPostHeader(update);
 		post ~= new Document(c);
 	} catch(CheckException e) {
 		post ~= new Element("p", "Looks like this post isn't proper XML!");
@@ -98,5 +105,27 @@ Element formatUpdate(Update update, string wrapperClass = "update") {
 	}
 
 	return post;
+}
+
+Element formatPostHeader(Update update) {
+	if(update is null)
+		return null;
+
+	Element postHeader = new Element("h3");
+	postHeader ~= new Document("<span>&#160;</span>");
+		postHeader.tag.attr["class"] = "uhead";
+	Element pTitle;
+	if(update.title.length)
+		pTitle = new Element("a", " " ~ update.title);
+	else
+		pTitle = new Element("a", " " ~ to!string(update.number));
+	pTitle.tag.attr["class"] = "utitle lcol";
+	pTitle.tag.attr["href"] = 
+		URL_BASE ~ URL_PREFIX ~ to!string(update.number);
+	Element pDT = new Element("span", update.date ~ " " ~ update.time);
+		pDT.tag.attr["class"] = "udt rcol";
+	postHeader ~= pTitle;
+	postHeader ~= pDT;
+	return postHeader;
 }
 
