d71d1f3ebc21f1dcb1f0affaac67caccc82350df
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-31 12:56:50 -0400

    Switch from different pages to just js tabs in about page

--- src/about_handler.d
+++ src/about_handler.d
@@ -19,47 +19,6 @@ Element aboutHandler(string URL) {
 	}
 
 	mBody ~= dumpFile(ABOUT_FILE);
-
-	Element links = new Element("p");
-		links.tag.attr["style"] = "text-align:center";
-	Element workLink = new Element("a", "Work");
-		workLink.tag.attr["href"] = URL_BASE ~ URL_PREFIX ~ "work";
-	Element hobbiesLink = new Element("a", "Hobbies");
-		hobbiesLink.tag.attr["href"] = URL_BASE ~ URL_PREFIX ~ "hobbies";
-	Element schoolLink = new Element("a", "School");
-		schoolLink.tag.attr["href"] = URL_BASE ~ URL_PREFIX ~ "school";
-	Element gradesLink = new Element("a", "Grades");
-		gradesLink.tag.attr["href"] = URL_BASE ~ URL_PREFIX ~ "grades";
-
-	string spaces = "&#160;"; spaces ~= spaces; spaces ~= spaces;
-	Document tab = new Document("<span>" ~ spaces ~ "</span>");
-
-	links ~= workLink; links ~= tab;
-	links ~= hobbiesLink; links ~= tab;
-	links ~= schoolLink; links ~= tab;
-	links ~= gradesLink;
-	mBody ~= links;
-
-	string[] fields = split(URL, "/");
-	if(fields.length > 1) {
-		switch(fields[1]) {
-			case "work":
-				mBody ~= dumpFile(ABOUT_FILE ~ "_work");
-				break;
-			case "hobbies":
-				mBody ~= dumpFile(ABOUT_FILE ~ "_hobbies");
-				break;
-			case "school":
-				mBody ~= dumpFile(ABOUT_FILE ~ "_school");
-				break;
-			case "grades":
-				mBody ~= dumpFile(ABOUT_FILE ~ "_grades");
-				break;
-			default:
-				mBody ~= new Element("p", "Bad sub-URL");
-		}
-	}
-
 	return mMBody;
 }
 
